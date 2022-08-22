08582fbec81bb74ff416d1fb529be6caf8ae2788
e <69575053+thatOneArchUser@users.noreply.github.com>
2022-08-22 15:24:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,24 @@ def encode(text):
 def decode(text):
     value = 0
     arr = []
+    out = str()
     for i in text:
         if i == "+": value += 1
         elif i == "-": value -= 1
-        elif i == ".":
-            print(''.join(arr))
-            arr = []
+        elif i == ".": out += ''.join(arr)
         elif i == "#":
             arr.append(chr(0x60+value))
             value = 0
         elif i == "@":
             arr.append(chr(0x40+value))
             value = 0
-        elif i == ";": quit()
+        elif i == ";": break
         elif i == "*": arr.append(" ")
         elif i == "!": value = 0
         elif i == "&": arr.append(str(value))
-    return
+    return ''.join(arr)
 
 if __name__ == "__main__":
     if sys.argv[1] == "encode": print(encode(sys.argv[2]))
-    elif sys.argv[1] == "decode": decode(sys.argv[2])
+    elif sys.argv[1] == "decode": print(decode(sys.argv[2]))
     else: print(f"Invalid arguments\nUsage: {sys.argv[0]} <encode/decode> <string>")
