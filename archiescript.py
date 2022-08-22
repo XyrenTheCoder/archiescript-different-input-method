37f200667e211adfa4d0543c7c97ff7c67dfd716
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-22 19:15:26 +0800

    Update archiescript.py

--- archiescript.py
+++ archiescript.py
@@ -1,35 +1,25 @@
 import os
-if os.name == "nt":
-    os.system("cls")
-else:
-    os.system("clear")
+if os.name == "nt": os.system("cls")
+else: os.system("clear")
 print("Archiescript v1.0\n")
 
 value = 0
 cmd = input(">> ")
 l = []
-for i in cmd:
-    l.append(i)
+for i in cmd: l.append(i)
 
 text = []
 for c in l:
-    if c == "+":
-        value += 1
-    elif c == "-":
-        value -= 1
-    elif c == ".":
-        print(''.join(text))
+    if c == "+": value += 1
+    elif c == "-": value -= 1
+    elif c == ".": print(''.join(text))
     elif c == '#':
         text.append(chr(0x60+value))
         value = 0
     elif c == '@':
         text.append(chr(0x40+value))
         value = 0
-    elif c == ";":
-        quit()
-    elif c == "*":
-        text.append(" ")
-    elif c == "!":
-        value = 0
-    elif c == "&":
-        text.append(str(value))
+    elif c == ";": quit()
+    elif c == "*": text.append(" ")
+    elif c == "!": value = 0
+    elif c == "&": text.append(str(value))
