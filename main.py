f7266dfcd54074b81bb7a9710e4a95849839b9c3
Isaac Dee Schnutz <69575053+thatOneArchUser@users.noreply.github.com>
2022-10-01 15:32:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def decode(text):
     return ''.join(arr)
 
 if __name__ == "__main__":
-    if len(sys.argv < 3):
+    if len(sys.argv) < 3:
         print("missing arguments")
         sys.exit(1)
     if sys.argv[1] == "encode": print(encode(sys.argv[2:]))
