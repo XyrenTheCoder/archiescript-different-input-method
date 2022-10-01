f835de45d6238e556fcd2bc5ffaef2b09bf34ce3
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-10-01 20:17:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os, sys, string
 
 def encode(text):
     arr = []
-    for i in text:
-        if i not in list(string.ascii_letters + string.digits): return f"Invalid character at position {text.index(i)}"
+    for i in " ".join(text):
+        if i not in list(string.ascii_letters + string.digits + " "): return f"Invalid character at position {text.index(i)}"
         if i == " ": arr.append("*")
         elif i.isupper(): arr.append(f"{(int(hex(ord(i)), 16) - int('0x40', 16))*'+'}@")
         elif i.islower(): arr.append(f"{(int(hex(ord(i)), 16) - int('0x60', 16))*'+'}#")
         elif i.isdigit:
-            var = "+"*int(i) + "&!"
+            var = "+"*int(i) + "%~"
             arr.append(var)
     arr.append(".;")
     return ''.join(arr)
@@ -29,11 +29,15 @@ def decode(text):
             value = 0
         elif i == ";": break
         elif i == "*": arr.append(" ")
-        elif i == "!": value = 0
-        elif i == "&": arr.append(str(value))
+        elif i == "~": value = 0
+        elif i == "%": arr.append(str(value))
     return ''.join(arr)
 
 if __name__ == "__main__":
-    if sys.argv[1] == "encode": print(encode(sys.argv[2]))
+    if sys.argv[1] == "encode": print(encode(sys.argv[2:]))
     elif sys.argv[1] == "decode": print(decode(sys.argv[2]))
     else: print(f"Invalid arguments\nUsage: {sys.argv[0]} <encode/decode> <string>")
+
+
+
+#
